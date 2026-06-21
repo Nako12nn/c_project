f4d9145e2a7ed200264d334fb4b45e38e804e434
Maxym <hako.diaha@gmail.com>
2026-06-21 14:40:34 +0200

    OOP in C

--- main.c
+++ main.c
@@ -1,15 +1,27 @@
 #include <stdio.h>
-#include "my_header1.h"
+#include <string.h>
+
+struct Car {
+    float speed;
+    float weight;
+    char model[16];
+};
 
 int main() {
     
-    float a = 3.28;
-    float b = 2.72;
-    add(a,b);
-    string("You didnt expect that \n");
+    struct Car mercedes_benz;
+    mercedes_benz.speed = 250.2f;
+    mercedes_benz.weight = 1700.0f;
+    strcpy(mercedes_benz.model, "Mercedes s500");
 
-    int result = mult(81, 3);
-    printf("%d\n", result);
+    struct Car bmw;
+    bmw.speed = 320.2f;
+    bmw.weight = 2000.0f;
+    strcpy(bmw.model, "BMW M6");
 
+    printf("%s has speed %.0fkm/h and weight %.0fkg\n", mercedes_benz.model, mercedes_benz.speed, mercedes_benz.weight);
+    printf("\n");
+    printf("%s has speed %.0fkm/h and weight %.0fkg\n", bmw.model, bmw.speed, bmw.weight);   
+    
     return 0;
 }
