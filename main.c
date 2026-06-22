5d90673066c7262620c1ed7b787c6c60a8cde1af
Maxym <hako.diaha@gmail.com>
2026-06-22 20:44:05 +0200

    %d and %c

--- main.c
+++ main.c
@@ -1,11 +1,43 @@
 #include <stdio.h>
+#include <string.h>
+
+
+void show_num(char x){
+    printf("%c \n", x);
+}
 
 int main() {
-    float fahr;
     
-    for (float fahr = 100; fahr > 0; fahr -= 2)
-    {
-        printf("%5.1f %7.1f\n", fahr, (5.0/9.0) * (fahr-32));
-    }
+    // char arr1 [] = "This file is mine ";
+    // for (int i = 0; i < strlen(arr1); i++)
+    // {
+    //     putchar(arr1[i]);
+    // }
+    // printf("\n");
+    // printf("%s", arr1);
+
+    // char new [] = getchar();
+    // for (int i = 0; i < strlen(new); i++)
+    // {
+    //     putchar(new[i]);
+    // }
+    // printf("Enter text: ");
+    // int symbol;
+    // while (symbol != '\n')
+    // {
+    //     symbol = getchar();
+    //     putchar(symbol);
+    // }
+       show_num(65);
+
+    int x1 = getchar();
+
+    printf("This: %d %c\n", x1, x1);
+
+
+    //double fex;
+    //printf("The size of: %lu\n", sizeof(char));
+
+
     return 0;
 }
