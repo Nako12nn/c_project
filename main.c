61b72b34a7009e01c8b98605911a90cece98bc3f
Maxym <hako.diaha@gmail.com>
2026-06-22 12:28:11 +0200

    Fahr to temp - qiuck code

--- main.c
+++ main.c
@@ -1,23 +1,11 @@
 #include <stdio.h>
 
 int main() {
-
-
-    int fahr, celsius;
-    int lower, upper, step;
-
-    lower = 0;
-    upper = 100;
-    step = 2;
-
-    fahr = lower;
-    while (fahr<=upper)
-    {
-        celsius = 5 * (fahr-32)/9;
-        printf("%d = %d \n", fahr, celsius);
-        fahr = fahr + step;
-    }
+    float fahr;
     
-
+    for (float fahr = 100; fahr > 0; fahr -= 2)
+    {
+        printf("%5.1f %7.1f\n", fahr, (5.0/9.0) * (fahr-32));
+    }
     return 0;
 }
