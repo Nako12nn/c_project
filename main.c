de93e3091b18b3e7ea6eb4eb08d256b776638935
Maxym <hako.diaha@gmail.com>
2026-06-24 20:50:00 +0200

    Switch and case

--- main.c
+++ main.c
@@ -3,18 +3,40 @@
 
 int main(void) {
 
-    float x = -6.5, y = 4.6;
-    float max_num = (x > y) ? x : y; // Ternary operation:  expression ? expression : expression
-    printf("The max: %.2f\n", max_num);
-    float res = (x < y) ? fabs(x) : fabs(y); // returns the module 
-    printf("%04.2f\n", res);
-    short t = 2;
-    printf("%hd is %s number\n", t, (t % 2 == 0) ? "even" : "odd"); // You can use ternar operation as an argument in the functions 
+    short item;
+    printf("1 C\n"
+           "2 Python\n"
+            "3 Java\n"
+            "4 Rust\n"
+        );
+
+    if ((scanf("%hd", &item)) != 1)
+    {
+        printf("Error\n");
+        return 0;
+    };
+    switch (item)
+    {
+    case 1:
+        printf("Learn C\n");
+        break;
+    case 2:
+        printf("Learn Python\n"); // break переходить до іншого оператора 
+                                  // return просто завершує роботу цеї функції 
+        break;
+
+    case 3:
+        printf("Learn Java\n");
+        break;        
     
-    short v = 9, s = 8, g = 7;
-    short max = (v > s) ? ((v > g) ? v : g) : ((s > g) ? s : g); 
-    
-    printf("Max number: %hd\n", max);
+    case 4:
+        printf("Learn Rust\n");
+        break;        
+
+    default:
+        printf("Exit\n");
+        break;
+    }
 
     return 0;
 }
