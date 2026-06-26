fd7cf3217c4ffe25df4e8b71f1eeaf0e8caa80c4
Maxym <hako.diaha@gmail.com>
2026-06-26 12:11:09 +0200

    Discriminant

--- main.c
+++ main.c
@@ -1,28 +1,32 @@
 #include <stdio.h>
-#include <stdlib.h> // lib for rand 
-#include <time.h>
+#include <math.h>
 
 int main(void) {
 
-    // time_t t = time(NULL);
-    srand(time(NULL));
-    printf("Before: %ld\n\n", time(NULL)); // time(NULL) shows current time on the cmpt
+    double a, b, c;
+    double D, x1, x2;
 
-    unsigned long my_time = time(NULL);
+    printf("Enter the values for a, b and c:\n");
+    if (scanf("%lf %lf %lf", &a, &b, &c) != 3)
+    {
+        printf("Enter correct values\n");
+        return 0;
+    }
+    
+    D = b * b - 4 * a * c;
+    printf("The discriminant = %.2lf\n", D);
 
-    unsigned long sec = my_time % 60;
-    unsigned long min = (my_time / 60) % 60;
-    unsigned long hour = my_time / (3600) ;
+    if (D < 0)
+    {
+        printf("The discriminant is less than 0, therefore there is no x's\n");
+        return 0;
+    }
+    
+    D = sqrt(D); // it dives square of num -> 25 = 5; 16 = 4;
 
-    printf("%02lu:%02lu:%02lu\n\n", hour, min, sec);
-
-    int guess1 = rand() % 10 + 1;
-    int guess2 = rand() % 10;
-    unsigned int guess3 = rand() + rand();
-    printf("%d\n%d\n%u\n", guess1, guess2, guess3);
-
-    double range_float = (double) rand() / (double) rand();
-    printf("%.10f\n", range_float);
+    x1 = (-b + D) / 2 * a;
+    x2 = (-b - D) / 2 * a;
+    printf("X1 = %.2lf\nX2 = %.2lf\n", x1, x2);
 
     return 0;
 }
