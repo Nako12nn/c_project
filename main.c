03e394a010d793286a8636db3bca46feb738cb99
Maxym <hako.diaha@gmail.com>
2026-06-27 12:01:40 +0200

    For in for and do while

--- main.c
+++ main.c
@@ -1,34 +1,56 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <math.h>
 
 int main(void) {
-    // int m, n;
-    // if (scanf("%d", &n) != 1) return 0;
-
-    // for (m = 0; n > 0; --n) 
-    // {
-    //     m += (n * n);
-    // }
-    // printf("%d\n", m);
-
-
-
-    // int till;
-    // if (scanf("%d", &till) != 1 || till > 20) return 0;
-
-    // long long fuct = 1;
-
-    // for (int i = 1; i <= till; fuct *= i, ++i)
-    // { }
     
-    // printf("%lld\n", fuct);
+    // short pass_code = 121;
+    // short taken_code;
+    // int buf;
+
+    // do {
+    //     printf("Enter the pass code: ");
+    //     if(scanf("%hd", &taken_code) != 1) return 0;
+
+    //     while ((buf = getchar()) != '\n' && buf != EOF)
+    //     { }
+        
+    // } while (taken_code != pass_code);
+    // printf("Access allowed ->");
 
 
-    for (int i = rand() % 10; i != 0; i = rand() % 10)
+    // short total_boxes = 3;
+    // short total_files = 4;
+
+    // for (short i = 0; i < total_boxes; i++)
+    // {
+    //     printf("///Box %hd\n", i+1);
+    //     for (short j = 0; j < total_files; j++)
+    //     {
+    //         printf("File %hd\n", j+1);
+    //     }
+    //     printf("\n");
+    // }
+    
+
+
+    int summa = 0;
+    short i_end = 7;
+    short j_end = 5;
+    short k_end = 4;
+
+    for (short i = -3; i <= i_end; i++)
     {
-        printf("i = %d\n", i);
+        for (short j = 1; j <= j_end; j++)
+        {
+            for (short k = 2; k <= k_end; k++)
+            {
+                summa += (i + j - k) * (i + j - k);
+            }
+            
+        }
+        
     }
     
-
+    printf("%d\n", summa);
     return 0;
 }
