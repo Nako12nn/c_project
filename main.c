5e76805034fa799a87ac188fba52c1a7a4abbaaa
Maxym <hako.diaha@gmail.com>
2026-06-29 10:54:36 +0200

    poiters3

--- main.c
+++ main.c
@@ -2,92 +2,81 @@
 
 int main(void) { 
     
-    // short my_num = 100;
-    // short *your_num = &my_num; // initialization of pointer
+    // Some problems with the poiters -> call to undefined area of memory
+    // long *serf; // wrong
+    // *serf = 2;
 
-    // printf("//// Example how to call poiter: \n");
-    // printf("The value from variable(my_num) = %hd\n", my_num);
-    // printf("The value from pointer(*your_num) = %hd\n", *your_num);
-    // printf("The adress from pointer(your_num) = %p\n\n", your_num);
+    // long mystr = 21;
+    // long *newpr = NULL; // how to check if a pointer is initialized and points to a specific memory location
 
-    // *your_num = 7; // change of my_num value and an assignment operations takes place here
-
-    // printf("//// Example after a change: \n");
-    // printf("2 The value from variable(my_num) = %hd\n", my_num);
-    // printf("2 The value from pointer(*your_num) = %hd\n", *your_num);
-    // printf("2 The adress from pointer(your_num) = %p\n\n", your_num);
-/// the examples show us that adress of cell stays the same, we can change value in that cell using *ptr
-    
-
-    // int stan_num = 19;
-    // int *stan_ptr, *pre_ptr;
-
-    // stan_ptr = &stan_num; // assign to stan_ptr adress of stan_num, 
-    // pre_ptr = stan_ptr; // assign to pre_ptr adress of stan_ptr which refers to adress of stan_num
-
-    // printf("//// Example3: \n");
-    // printf("3 The value from variable(stan_num) = %d\n", stan_num);
-    // printf("3 The adress from pointer(stan_ptr) = %p\n", stan_ptr);
-    // printf("3 The adress from pointer(pre_ptr) = %p\n", pre_ptr);
-    // printf("3 The value from pointer(*stan_ptr) = %d\n", *stan_ptr);
-    // printf("3 The value from pointer(*pre_ptr) = %d\n\n", *pre_ptr);
-
-    // *stan_ptr = 22;
-    // printf("4 The value from variable(stan_num) = %d\n", stan_num); // as we changed the value by means of *stan_ptr it = 22
-    // printf("4 The adress from variable(stan_num) = %p\n\n", &stan_num); // stays the same 
+    // if (newpr != NULL) { // that is how to check it, but it doesnt work in 100 %
+    // *newpr = 45;
+    // printf("the value = %ld\n\n", *newpr);
+    // }
 
 
-    // *pre_ptr = 13;
-    // printf("5 The value from variable(stan_num) = %d\n", stan_num); // as we changed the value by means of *pre_ptr it = 13
-    // printf("5 The adress from variable(stan_num) = %p\n", &stan_num); // stays the same
-/// we can change the value using *pointer = x;
+    // int nice = 7;
+    // int *ptr_nice = &nice;
+
+    // printf("the adress of int ptr_nice = %u\n", ptr_nice);  
+
+    // ptr_nice++; /// the difference - 4, cause int has 4 bytes, and we here wokr with data portions
+    // printf("the adress of int ptr_nice = %u\n\n", ptr_nice); 
 
 
-    int newr = 777;
-    int *firs;
-    char *secd;
+    // char try = 9;
+    // char *ptr_try = &try;
 
-    firs = &newr;
-    secd = (char *)firs; // there was:  secd = firs;  the program works in the same way -> 769
-    //// Type conversion should be performed when the types are different 
+    // printf("the adress of char ptr_try = %u\n", ptr_try);
 
-    printf("The value from variable(newr) before = %d\n\n", newr); 
-    /* int has 4 bytes(cells) and char - 1; initially the number 777 is represented in bytes like this 9 * 256^0 + 
-    + 3 * 256^1 + 0 * 256^2 + 0 * 256^3 == 777 
-    and when we using CHAR secd assign to newr 1 it is meant that in the first cell is put 1 ->
-    __1__  * 256^0 + 3 * 256^1 + 0 * 256^2 + 0 * 256^3 == 769 */
-    
-    *secd = 1;
-    printf("The value from variable(newr) after = %d\n\n", newr);
-
-    /*if we need assign to pointer another one with other type we have to convert the types to a single type 
-    example:
-    int some = 120;
-    int *myptr = &some;
-    char *newptr;
-||  newptr = (char *)myptr
-    *newptr = 99;          */
-
-    
-    // in this case type conversion should not be performed
-    int initl = 25;
-    int *thisp = &initl;
-    short *nbv;
-    void *p;
-
-    p = thisp;
-    nbv = p;
-
-    //printf("the adress = %d\n", *p);  you must not use void ptrs for working with cells only with adresses
-    printf("the adress = %p\n", p);
-    printf("the value = %d\n", *nbv);
-
-    *nbv = 36;
-
-    printf("the value = %d\n", *nbv);
+    // ptr_try++; /// char has 1 byte so the same is here -> data portions, not just numbers
+    // printf("the adress of char ptr_try = %u\n\n", ptr_try);
 
 
 
-    
+    // short arr[8];
+    // short *ptr_arr = arr; /// *ptr_arr refers to the first element of this array
+    // short *ptr2_arr = &arr[3]; /// *ptr2_arr refers to the fourth element 
+
+    // printf("the adress of ptr_arr = %u\n", ptr_arr);
+    // printf("the adress of ptr2_arr = %u\n\n", ptr2_arr);
+
+    // int res = ptr2_arr - ptr_arr; // this operation: (&ptr2 - &ptr) / 2 (sizeof short)
+    // printf("The res = %d\n", res); // it shows how many elements are between ptr2 - ptr
+
+
+    // int lak = 476789;
+    // char *ptr_lak = (char *)&lak; /// char ptr refers to the first (one) byte of lak which has 4 ones 
+    // int *ptr_int = &lak;
+
+    // printf("int ptr = %d\n", *ptr_int);   
+    // printf("Char ptr = %d\n", *ptr_lak); // the first byte is printed 117 * 256^0
+
+    // ptr_lak++;
+    // printf("Char ptr = %d\n", *ptr_lak); // the second one 70 * 256^1 and so on
+
+
+    // // for (int i = 0; i < sizeof(int); ++i)
+    // //     printf("%d ", *ptr_lak++);   *ptr_lak++ == *(ptr_lak++)
+
+
+    // int lak = 476789;
+    // int *ptr_int = &lak;
+
+    // *ptr_int += 1;
+    // printf("%d\n", *ptr_int); // 1 - *ptr_int, 2 - + 1;
+
+
+    int nwq = 476789;
+    char *my_ptr = (char *) &nwq;
+
+    int x = *my_ptr + 1; // 1 - read value of *my_ptr  2 - + 1,  3 - x = that value
+    printf("x = %d\nnwq = %d\n", x, nwq);
+
+    int y = *my_ptr++;
+    printf("y = %d\nnwq = %d\n", y, nwq);
+
+
+    printf("\n");
     return 0;
 }
