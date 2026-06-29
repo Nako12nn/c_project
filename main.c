f7a3e91bbb31ce5197464f62d314869bdedf53c2
Maxym <hako.diaha@gmail.com>
2026-06-29 12:33:49 +0200

    Arrays 2

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 int main(void) { 
 /// An array is not a full-fledged data structure, 
 /// but rather a continuous memory area for storing a collection of data of the same type 
+/// If a program crashes even once out of a million runs, it's always the programmer's fault.
 
     int marks[TOTAL_MARKS];
 
@@ -13,6 +14,11 @@ int main(void) {
 
     int x1 = marks[9];
     printf("x1 = %d\n\n", x1);
+
+    size_t bytes_marks = sizeof(marks);
+    size_t size_marks = sizeof(marks) / sizeof(marks[0]); // marks[0] == *marks
+        printf("size of marks: %zu\n", size_marks); // shows amount of elements
+        printf("amount of used bytes in marks: %zu\n", bytes_marks); // shows an amount of used bytes
 
     for (int i = 0; i < TOTAL_MARKS; i++)
     {
