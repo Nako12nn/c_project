c5f263c775019b89f1466b4a7ee3b16a9b90ed58
Maxym <hako.diaha@gmail.com>
2026-06-30 16:59:58 +0200

    Arrays 3

--- main.c
+++ main.c
@@ -2,29 +2,17 @@
 #define TOTAL_MARKS   10
 
 int main(void) { 
-/// An array is not a full-fledged data structure, 
-/// but rather a continuous memory area for storing a collection of data of the same type 
-/// If a program crashes even once out of a million runs, it's always the programmer's fault.
 
-    int marks[TOTAL_MARKS];
+    int marks[TOTAL_MARKS] = {1, 2, 3, 4, 5}; // it's initialization not assignment
+    marks[0] = 99; // it's assignment
+    
+    int new_arr[] = {21, 34, -3};
+    int old_arr[] = {1, [5] = 31, 9, [9] = 22};
 
-    marks[9] = 75;
-    marks[0] = 80;
-    marks[4] = 60;
-
-    int x1 = marks[9];
-    printf("x1 = %d\n\n", x1);
-
-    size_t bytes_marks = sizeof(marks);
-    size_t size_marks = sizeof(marks) / sizeof(marks[0]); // marks[0] == *marks
-        printf("size of marks: %zu\n", size_marks); // shows amount of elements
-        printf("amount of used bytes in marks: %zu\n", bytes_marks); // shows an amount of used bytes
-
-    for (int i = 0; i < TOTAL_MARKS; i++)
+    for (int i = 0; i < sizeof(old_arr) / sizeof(*old_arr); i++)
     {
-        printf("N_%d of the array marks: %d\n", i+1, marks[i]);
+        printf("N_%d of the array marks: %d\n", i+1, old_arr[i]);
     }
     
-
     return 0;
 }
