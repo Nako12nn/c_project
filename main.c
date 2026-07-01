be360d29ef0b9ae13b7ea40b1f4523ae284061e1
Maxym <hako.diaha@gmail.com>
2026-07-01 14:20:38 +0200

    strings

--- main.c
+++ main.c
@@ -1,37 +1,30 @@
 #include <stdio.h>
 
 int main(void) { 
+    
+//     char str[] = {'N', 'a', 'k', 'o', 'n', 'e', 'c', 'h', 'n', 'y', 'i', '\0'};// \0 shows the end of string!
+//     char s[] = "Maksym Nakonechnyi";
+//     char buffer[32] = "Hello, guys";
 
-    int game_pole[4][5];
+//     char b[] = "The" "re"
+//             " we go";
 
-    size_t bytes = sizeof(game_pole);
-    printf("all bytes = %zu\n", bytes);
+//     char ni[] = "how are \
+// you";
 
-    size_t row_bytes = sizeof(game_pole[0]);
-    printf("row bytes = %zu\n", row_bytes);
+    // const char *ptr_s = "Nakonechnyi"; //read-only, it is placed in unchangable memory area
+    // char s[] = "Nakonechnyi"; // can change
 
-    size_t rows = sizeof(game_pole) / sizeof(game_pole[0]);
-        printf("rows = %zu\n", rows);
-
-    size_t columns = sizeof(game_pole[0]) / sizeof(game_pole[0][0]);
-        printf("columns = %zu\n\n", columns);
+    // //ptr_s[1] = 'n'; error
+    // s[1] = 'A';
 
 
 
-    char array[3][3] = {9, 7, 1, 4, 5, 6, 7, 8, 9};
-    char *ptr_array = array[1];
+    char d[] = "You like program in \"C\" Language?";
+    puts(d);
 
-    printf("%d\n", *(ptr_array - 2));
-
-    char (*ptr_arr2)[3] = array;// ptr on 2d array
-    char *ptr_arr3[3]; // array with 3 poiters
-
-    char x = ptr_arr2[0][1];
-    printf("x = %d\n", x);
-
-    ptr_arr2[0][0] = 0;
-    printf("%d\n", ptr_arr2[0][0]);
-
+    char string[] = "\x61 \x4f";
+    puts(string);
 
     return 0;
 }
